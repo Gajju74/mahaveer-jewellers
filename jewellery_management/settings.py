b6da794de47cae99c66aa92cee82c99132d36abb
Gajju74 <gbanjara852@gmail.com>
2024-09-21 10:57:15 +0530

    add allowed host

--- jewellery_management/settings.py
+++ jewellery_management/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mahaveer-jewellers-test.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
